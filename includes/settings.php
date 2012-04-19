f5b3f623750683ffc7b07502352610be20886037
Matiss Rikters <matiss.tk@gmail.com>
2012-04-19 16:59:50 +0300

    /

--- includes/settings.php
+++ includes/settings.php
@@ -1,8 +1,2 @@
 <?php return array (
-  'db_server' => 'localhost',
-  'db_user' => 'root',
-  'db_password' => 'root',
-  'db_database' => 'riks',
-  'twitter_username' => 'hah_lv',
-  'twitter_password' => 'baumas10',
 ) ;
