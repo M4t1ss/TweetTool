db388d4b77a7c11aede46a2af52d9fccf7dd4ec2
Matiss Rikters <matiss.tk@gmail.com>
2012-04-28 15:10:07 +0300

    clear this...

--- includes/settings.php
+++ includes/settings.php
@@ -1,13 +1,2 @@
 <?php return array (
-  'db_server' => 'localhost',
-  'db_user' => 'root',
-  'db_password' => 'root',
-  'db_database' => 'riks_test',
-  'twitter_username' => 'hah_lv',
-  'twitter_password' => 'baumas10',
-  'start_time' => 1335614438,
-  'end_time' => 1335614638,
-  'replace_usernames' => 'y',
-  'replace_hashtags' => 'n',
-  'replace_links' => 'y',
 ) ;
